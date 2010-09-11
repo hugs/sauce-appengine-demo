c62368781d5c22ad429d8c4810309f387eb674ca
Jason Huggins <hugs@saucelabs.com>
2010-09-10 20:49:45 -0700

    Um, yeah... don't check in passwords...

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-basic_auth_name     = 'admin'
-basic_auth_password = 'foobar'
-sauce_username      = 'test-account-123'
-sauce_accesskey     = '88a3c637-e45e-451a-8496-52a3543cfdac'
+basic_auth_name     = ''
+basic_auth_password = ''
+sauce_username      = ''
+sauce_accesskey     = ''
 
